577f594c8b2e28c20b92f45822c7378a68bb58b6
Michael Fiano <michael.fiano@gmail.com>
2015-02-07 08:05:22 -0500

    print literal key names to debug log

--- input.lisp
+++ input.lisp
@@ -5,13 +5,17 @@
 (defgeneric mouse-down (button coords &key))
 (defgeneric mouse-up (button coords &key))
 
+(defun get-key-name (scancode)
+  (let ((key (sdl2-ffi.functions:sdl-get-key-from-scancode scancode)))
+    (sdl2-ffi.functions:sdl-get-key-name key)))
+
 (defmethod key-down :after (scancode repeat-p &key (debugp nil))
   (when (and (not repeat-p) debugp)
-    (format t "Key pressed: ~a~%" scancode)))
+    (format t "Key pressed: ~a~%" (get-key-name scancode))))
 
 (defmethod key-up :after (scancode repeat-p &key (debugp nil))
   (when (and (not repeat-p) debugp)
-    (format t "Key released: ~a~%" scancode)))
+    (format t "Key released: ~a~%" (get-key-name scancode))))
 
 (defmethod mouse-down :after (button coords &key (debugp nil))
   (when debugp
